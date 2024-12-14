7f0e999eaf1fa9e8e8cb98ef9d7340a67992beac
Rosemilson <rozbr96@gmail.com>
2024-12-14 01:45:37 -0300

    run rspec if a ruby file

--- lua/spec_runner/init.lua
+++ lua/spec_runner/init.lua
@@ -6,9 +6,26 @@ local function run_shell_command(cmd)
   print(output)
 end
 
+local function get_ruby_spec_command()
+  local file = vim.fn.expand('%')
+  if vim.fn.filereadable(file) == 1 then
+    return 'rspec ' .. file
+  else
+    return "echo 'Open a file first, dumbass'"
+  end
+end
+
 function spec_runner.run_specs()
-  local default_command = "echo 'Running specs...'"
-  run_shell_command(default_command)
+  local filetype = vim.bo.filetype
+  local command
+
+  if filetype == 'ruby' then
+    command = get_ruby_spec_command()
+  else
+    command = "echo 'Unsupported filetype: " .. filetype .. "'"
+  end
+
+  run_shell_command(command)
 end
 
 function spec_runner.setup()
